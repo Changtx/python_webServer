2ff25432710558e6f5c412503496c2a17300d085
changtx <changtx1991@gmail.com>
2016-07-06 23:45:18 -0500

    add wsgi config & logic for server

--- wsgiServer/webServer2.py
+++ wsgiServer/webServer2.py
@@ -29,3 +29,71 @@ class WSGIServer(object):
         # Return headers set by Web framework/Web application
         self.headers_set = []
 
+    def set_app(self, application):
+        self.application = application
+
+    def server_forever(self):
+        listen_socket = self.listen_socket
+        while True:
+            # New client connection
+            self.client_connection, client_address = listen_socket.accept()
+            # Handle one request and close the client connection. Then
+            # loop over to wait for another client connection
+            self.handle_one_request()
+
+    def handle_one_request(self):
+        self.request_data = request_data = self.client_connection.recv(1024)
+        # Print formateed request data a la 'curl -v'
+        print(''.join(
+            '<{line}\n'.format(line=line)
+            for line in request_data.splitlines()
+        ))
+        
+        self.parse_request(request_data)
+        
+        # Construct environment dictionary using request data
+        env = self.get_environ()
+        
+        # It is time to call our application callable and get 
+        # back a result that will become HTTP response body
+        result = self.application(env, self.start_response)
+        
+        # Construct a response and send it back to the client
+        self.finish_response(result)
+        
+    def parse_request(self, text):
+        request_line = text.splitlines()[0]
+        request_line = request_line.rstrip('\r\n')
+        # Break down the request line into components
+        (self.request_method,   # Get
+         self.path,             # /hello
+         self.request_version   # HTTP/1.1
+         ) = request_line.split()
+
+    def get_environ(self):
+        env = {}
+        # The following code snippet does not follow PEP8 conventions
+        # but it's formatted the way it is for demonstration purposes
+        # to emphasize the required variables and their value
+        #
+        # Required WSGI variables
+        env['wsgi.version'] = (1, 0)
+        env['wsgi.url_scheme'] = 'http'
+        env['wsgi.input'] = StringIO.StringIO(self.request_data)
+        env['wsgi.errors'] = sys.stderr
+        env['wsgi.multithread'] = False
+        env['wsgi.multiprocess'] = False
+        env['wsgi.run_once'] = False
+        # Required CGI variables
+        env['REQUEST_METHOD'] = self.request_method     # GET
+        env['PATH_INFO'] = self.path                    # /hello
+        env['SERVER_NAME'] = self.server_name           # localhost
+        env['SERVER_PORT'] = str(self.server_port)      # 8888
+        return env
+
+    def start_response(self, status, response_headers, exc_info=None):
+        pass
+
+    def finish_response(self, result):
+        pass
+        
